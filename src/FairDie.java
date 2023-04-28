3dea472521150c5ae57cae4eec7a85f4079c6d1d
Peter <phildorf@gmail.com>
2023-04-28 10:39:42 +0200

    Opgave 4 - Lav 2 terning som kan bruges med en abstract metode int rul() og hvis man slår højere end 10 skal man slå igen

--- src/FairDie.java
+++ src/FairDie.java
@@ -1,7 +1,10 @@
-public class Terning {
+public class FairDie {
     private int antalsider;
 
-    public class terning(int antalsider){
+    public FairDie(int antalsider){
         this.antalsider = antalsider;
+    }
+    public int rul(){
+        return (int) (Math.random() * antalsider) + 1;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        /**
+         * Opgave 4 lav 2 terninger som kan bruges med en abstract metode int rul() og hvis man slår højere end 10 skal man slå igen
+         */
+        Scanner scan = new Scanner(System.in);
+        FairDie terning = new FairDie(6);
+        FairDie terning2 = new FairDie(6);
+        int resultat = terning.rul()+ terning2.rul();
+
+        System.out.println("Tryk k for at kaste terninger");
+        scan.next();
+        System.out.println("Du slog: "+resultat);
+
+
+        while(true){
+            if (resultat < 10){
+                System.out.println("Du slog mindre end 10, din tur slutter");
+                break;
+            }else if(resultat > 10){
+                System.out.println("Du slog højere end 10, slå igen");
+                scan.next();
+               terning = new FairDie(6);
+               terning2 = new FairDie(6);
+               resultat = terning.rul()+ terning2.rul();
+                System.out.println(resultat);
+            }
+        }
+    }
 }
